2ae6ddd6987f5bf8c8f3dc84f7b968371a892c08
Andrew Duthie <andrew@andrewduthie.com>
2017-08-09 18:51:51 -0400

    Utils: Add focusable search utilities

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,8 @@
+import * as focus from './focus';
 import * as keycodes from './keycodes';
 import * as nodetypes from './nodetypes';
 
+export { focus };
 export { keycodes };
 export { nodetypes };
 
