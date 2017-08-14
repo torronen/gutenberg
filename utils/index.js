5ce8b15a7d695870a3da029a06511701d63d37c9
Andrew Duthie <andrew@andrewduthie.com>
2017-08-14 15:40:07 -0400

    Components: Use ally.js for first tabbable selection

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,6 @@
-import * as focus from './focus';
 import * as keycodes from './keycodes';
 import * as nodetypes from './nodetypes';
 
-export { focus };
 export { keycodes };
 export { nodetypes };
 
